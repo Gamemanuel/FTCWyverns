6d8fe9847019f61dd214fb31836e66af2dc0c4e2
Gamemanuel <163799483+Gamemanuel@users.noreply.github.com>
2024-09-03 17:03:22 -0700

    main v2

--- Main.java
+++ Main.java
@@ -7,8 +7,7 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 
 public class Main extends LinearOpMode {
     // makes it so i can access the the motors from different children classes
-    //mapping
-    //         ______                    ______
+//           ______                    ______
 //            |      |  _____    _____  |      |
 //            |      |-|M(L1)|  |M(R1)|-|      |
 //            |______| |_____|  |_____| |______|
@@ -18,12 +17,10 @@ public class Main extends LinearOpMode {
 //            |      | |M(L2)|  |M(R3)| |      |
 //            |      |-|_____|  |_____|-|      |
  //           |______|                  |______|
-
-    
-        DcMotor left1;
-        DcMotor left2;
-        DcMotor right1;
-        DcMotor right2;
+    DcMotor left1;
+    DcMotor left2;
+    DcMotor right1;
+    DcMotor right2;
     @Override
     public void runOpMode() throws InterruptedException {
         // sets the different motors
@@ -31,48 +28,48 @@ public class Main extends LinearOpMode {
         left2 = hardwareMap.get(DcMotor.class, "left2");
         right1 = hardwareMap.get(DcMotor.class, "right1");
         right2 = hardwareMap.get(DcMotor.class, "right2");
+
+        waitForStart();
         //use the following bit of code ONLY of your motors do not all go forwards
-            //left1.setDirection(DcMotor.Direction.REVERSE);
-            //left2.setDirection(DcMotor.Direction.REVERSE);
-            //right1.setDirection(DcMotor.Direction.FORWARD);
-            //right2.setDirection(DcMotor.Direction.FORWARD);
+        //left1.setDirection(DcMotor.Direction.REVERSE);
+        //left2.setDirection(DcMotor.Direction.REVERSE);
+        //right1.setDirection(DcMotor.Direction.FORWARD);
+        //right2.setDirection(DcMotor.Direction.FORWARD);
 
         //tank drive setting
         //spinleft(gamepad1.left_stick_y);
         // the power has to be between -1 and 1
         // this spins the 2 left motors for the same power
         //spinright(gamepad1.right_stick_y);
-    // one joystick controls turning and going straight
+        // one joystick controls turning and going straight
         //straight and backwards
-        wholedrivetrain((-1 * gamepad1.left_stick_y)); // Note: pushing stick forward gives negative value
-        //turning
-            //left
-        spinleft((-1 * gamepad1.left_stick_x));
-            //right
-        spinright(gamepad1.left_stick_x);
-    // strafing ( mecanum only )
-        strafeleft(gamepad1.left_bumper);
-        straferight(gamepad1.right_bumper);
+
+        while(opModeIsActive()) {
+
+
+            //wholedrivetrain((-1 * gamepad1.left_stick_y)); // Note: pushing stick forward gives negative value
+            //turning
+            spin(gamepad1.left_stick_x);
+            // strafing ( mecanum only )
+            //strafeleft(gamepad1.left_bumper);
+            //straferight(gamepad1.right_bumper);
+        }
     }
-    public void spinleft ( double leftpower) {
+    public void spin ( double leftpower) {
         // turns the 2 individual motors into a motor group
         left1.setPower(leftpower);
         left2.setPower(leftpower);
-    }
-    public void spinright ( double rightpower) {
-        // turns the 2 individual motors into a motor group ( right side )
-        right1.setPower(rightpower);
-        right2.setPower(rightpower);
+        right1.setPower(-leftpower);
+        right2.setPower(-leftpower);
     }
     public void wholedrivetrain ( double power) {
-        spinright(power);
-        spinleft(power);
+        spin(power);
     }
     // the following functions are for mecanums strafing
     public void strafeleft (boolean straifleft) {
         boolean True = true;
         if (straifleft == True); {
-           straifmotion(1);
+            straifmotion(1);
         }
 
     }
